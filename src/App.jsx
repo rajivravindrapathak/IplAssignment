78a31941a6db91b2903ed84604cf7209f9444d60
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2022-10-14 08:13:43 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Ipl from './Components/Ipl'
+
+function App() {
+
+  return (
+    <div className="App">
+     <Ipl />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Ipl.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Ipl = () => {
+  return (
+    <div>
+      hello ipl
+    </div>
+  )
+}
+
+export default Ipl
